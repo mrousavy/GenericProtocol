4a0e2112cdd217f87db2e6e3150721fef038e47d
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 18:00:36 +0200

    Add Test Project

---
+++ GenericProtocolTest/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using GenericProtocol.Implementation;
+
+namespace GenericProtocolTest {
+    public class Program {
+        private static ProtoServer<string> _server;
+
+
+        private static void Main(string[] args) {
+            StartServer();
+            StartClient();
+
+            while (true)
+            Console.ReadKey();
+        }
+
+
+        private static void StartClient() {
+            //var client = new GenericProtocol.Implementation.ProtoClient<string>(IPAddress.Any, 1024);
+            //client.Start().GetAwaiter().GetResult();
+        }
+
+        private static void StartServer() {
+            _server = new ProtoServer<string>(IPAddress.Any, 1024);
+            Console.WriteLine("Starting Server...");
+            _server.Start().GetAwaiter().GetResult();
+            Console.WriteLine("Server started!");
+            _server.ClientConnected += ClientConnected;
+            _server.ReceivedMessage += MessageReceived;
+        }
+
+        private static void MessageReceived(IPAddress sender, string message) {
+            Console.WriteLine($"{sender}: {message}");
+        }
+
+        private static void ClientConnected(IPAddress address) {
+            _server.Send("hello!", address);
+        }
+    }
+}
