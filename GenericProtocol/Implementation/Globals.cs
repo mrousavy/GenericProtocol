556a398ac454b6b762d803b199f07893743e1beb
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 20:33:46 +0200

    Refactor Events

--- GenericProtocol/Implementation/Globals.cs
+++ GenericProtocol/Implementation/Globals.cs
@@ -8,4 +8,9 @@ namespace GenericProtocol.Implementation {
     public delegate void ReceivedHandler<T>(
         IPAddress senderIp,
         T message);
+
+    public static class Constants {
+        public const int ReceiveBufferSize = 1024;
+        public const int MaxConnectionsBacklog = 10;
+    }
 }
