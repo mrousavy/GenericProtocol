96e5da88aefbf9e4f351d8f9a71865dcad5d562c
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 16:57:07 +0200

    Add Server implementation

--- GenericProtocol/IServer.cs
+++ GenericProtocol/IServer.cs
@@ -1,30 +1,40 @@
 ﻿using GenericProtocol.Implementation;
 using System;
-using System.Net.Sockets;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace GenericProtocol {
     public interface IServer<T> : IDisposable {
         event ClientContextHandler ClientConnected;
         event ClientContextHandler ClientDisconnected;
-        event ReceivedHandler<T> ReceivedMessage; 
+        event ReceivedHandler<T> ReceivedMessage;
 
         /// <summary>
         /// Bind the Socket to the set IP Address 
         /// and start listening for incoming connections
         /// </summary>
-        void Start();
+        Task Start();
 
         /// <summary>
         /// Stop the Server and disconnect all Clients
         /// gracefully
         /// </summary>
-        void Stop();
+        Task Stop();
 
         /// <summary>
         /// Send a new Message to the Client
         /// </summary>
         /// <param name="message">The message object
         /// to serialize and send to the client</param>
-        void Send(T message);
+        /// <param name="to">The client to send the 
+        /// message to</param>
+        Task Send(T message, IPAddress to);
+
+        /// <summary>
+        /// Broadcast a new Message to all connected Clients
+        /// </summary>
+        /// <param name="message">The message object
+        /// to serialize and send to the client</param>
+        Task Broadcast(T message);
     }
 }
---
+++ GenericProtocol/Implementation/ProtoServer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace GenericProtocol.Implementation {
+    public class ProtoServer<T> : IServer<T> {
+        #region Properties
+        public event ClientContextHandler ClientConnected;
+        public event ClientContextHandler ClientDisconnected;
+        public event ReceivedHandler<T> ReceivedMessage;
+
+        private IPEndPoint EndPoint { get; }
+        private Socket Socket { get; }
+        private IList<IPAddress> Clients { get; }
+        #endregion
+
+        #region ctor
+        /// <summary>
+        /// Create a new instance of the <see cref="GenericProtocol{T}"/>
+        /// with the default <see cref="AddressFamily"/> and <see cref="SocketType"/>.
+        /// Use <see cref="Start"/> to bind and start the socket.
+        /// </summary>
+        /// <param name="address">The <see cref="IPAddress"/> to start this Protocol on</param>
+        /// <param name="port">The Port to start this Protocol on</param>
+        public ProtoServer(IPAddress address, int port) : 
+            this(address, port, AddressFamily.InterNetwork, SocketType.Stream) { }
+
+        /// <summary>
+        /// Create a new instance of the <see cref="GenericProtocol{T}"/>.
+        /// Use <see cref="Start"/> to bind and start the socket.
+        /// </summary>
+        /// <param name="family">The <see cref="AddressFamily"/> 
+        /// this <see cref="System.Net.Sockets.Socket"/> should use</param>
+        /// <param name="type">The <see cref="SocketType"/> this 
+        /// <see cref="System.Net.Sockets.Socket"/> should use</param>
+        /// <param name="address">The <see cref="IPAddress"/> to start this Protocol on</param>
+        /// <param name="port">The Port to start this Protocol on</param>
+        public ProtoServer(IPAddress address, int port, AddressFamily family, SocketType type) {
+            Clients = new List<IPAddress>();
+            EndPoint = new IPEndPoint(address, port);
+            Socket = new Socket(family, type, ProtocolType.Tcp);
+        }
+        #endregion
+
+        /// <summary>
+        /// Bind and Start the Server to the set IP Address.
+        /// </summary>
+        public Task Start() {
+            return Task.Run(() => Socket.Bind(EndPoint));
+        }
+
+        /// <summary>
+        /// Shutdown the server and all active clients
+        /// </summary>
+        public Task Stop() {
+            throw new NotImplementedException();
+        }
+        
+        public Task Send(T message, IPAddress to) {
+            throw new NotImplementedException();
+        }
+
+        public Task Broadcast(T message) {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose() {
+            Socket?.Dispose();
+        }
+    }
+}
