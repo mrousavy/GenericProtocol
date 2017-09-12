05650367ff08c3579f58f0ea00543a436c1f6c1b
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 16:11:07 +0200

    Import ZeroFormatter and begin write interface

--- GenericProtocol/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GenericProtocol
-{
-    public class Class1
-    {
-    }
-}
---
+++ GenericProtocol/IServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericProtocol {
+    public interface IServer {
+
+        event EventHandler ClientConnected;
+
+        /// <summary>
+        /// Bind the Socket to the set IP Address 
+        /// and start listening for incoming connections
+        /// </summary>
+        void Start();
+    }
+}
