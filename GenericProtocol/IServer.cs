f33359d49f6da5fd4507c3715592fbd17a758ee9
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 16:44:40 +0200

    Extend interface

--- GenericProtocol/IServer.cs
+++ GenericProtocol/IServer.cs
@@ -1,16 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using GenericProtocol.Implementation;
+using System;
+using System.Net.Sockets;
 
 namespace GenericProtocol {
-    public interface IServer {
-
-        event EventHandler ClientConnected;
+    public interface IServer<T> : IDisposable {
+        event ClientContextHandler ClientConnected;
+        event ClientContextHandler ClientDisconnected;
+        event ReceivedHandler<T> ReceivedMessage; 
 
         /// <summary>
         /// Bind the Socket to the set IP Address 
         /// and start listening for incoming connections
         /// </summary>
         void Start();
+
+        /// <summary>
+        /// Stop the Server and disconnect all Clients
+        /// gracefully
+        /// </summary>
+        void Stop();
+
+        /// <summary>
+        /// Send a new Message to the Client
+        /// </summary>
+        /// <param name="message">The message object
+        /// to serialize and send to the client</param>
+        void Send(T message);
     }
 }
---
+++ GenericProtocol/Implementation/Events.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace GenericProtocol.Implementation {
+
+    public enum ClientContextAction {
+        /// <summary>
+        /// The Client just connected
+        /// </summary>
+        Connected,
+        /// <summary>
+        /// The Client disconnected or timed out
+        /// </summary>
+        Disconnected
+    }
+
+    public delegate void ClientContextHandler(
+        ClientContextAction action,
+        IPAddress clientIp);
+
+    public delegate void ReceivedHandler<T>(
+        T message,
+        IPAddress senderIp);
+}
