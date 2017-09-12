8c775184488e95a016df2a20afd7fe84d1580439
mrousavy <mrousavy@donotreply.github.com>
2017-09-12 17:24:44 +0200

    Add ping connection socket extension

---
+++ GenericProtocol/Implementation/SocketExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace GenericProtocol.Implementation {
+    public static class SocketExtensions {
+        public static bool IsConnected(this Socket socket) {
+            try {
+                return !(socket.Poll(1, SelectMode.SelectRead) && socket.Available == 0);
+            } catch (SocketException) { return false; }
+        }
+    }
+}
